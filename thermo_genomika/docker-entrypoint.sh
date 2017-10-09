454c31789c1ff75ca38522753d8ddb1f7dbb4e5a
joao uchoa <joao.victor@genomika.com.br>
2017-10-09 19:31:30 +0000

    Update docker-entrypoint.sh

--- thermo_genomika/docker-entrypoint.sh
+++ thermo_genomika/docker-entrypoint.sh
@@ -4,12 +4,8 @@ echo "America/Recife" > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
 
 /usr/sbin/sshd -D &!
-cd /var/www
 
-git clone git@gitlab.com:genomika/thermo-gnmk.git
-cd thermo-gnmk
 git checkout $thermo_gnmk_branch
-cd thermo_genomika
 
 make clean
 
