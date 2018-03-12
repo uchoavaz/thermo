73b1c8b62897f9836a93bdfb9022b61a1b25808e
joao uchoa <joao.victor@genomika.com.br>
2018-03-12 20:40:30 +0000

    correcao

--- thermo_genomika/core/tasks/periodic_tasks.py
+++ thermo_genomika/core/tasks/periodic_tasks.py
@@ -31,7 +31,7 @@ def check_device_status(thermo, device_line):
                 device_status.first_cursor = True
 
         device_status.save()
-        send_email = ( (not device_status.first_check) and (not device_status.second_check) or ( (not device_status.first_check) and device_status.second_check )
+        send_email = ( (not device_status.first_check) and (not device_status.second_check) or ( (not device_status.first_check) and device_status.second_check ))
 
         if device_status.first_check == False and device_status.second_check == True:
             message = "Dispositivo Online !"
