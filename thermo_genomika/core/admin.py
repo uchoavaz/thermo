5c45d2e151f31198f05f3c841ed933cabc982251
joao uchoa <joao.victor@genomika.com.br>
2018-03-12 15:42:19 +0000

    correcao

--- thermo_genomika/core/admin.py
+++ thermo_genomika/core/admin.py
@@ -5,4 +5,8 @@ class SystemInfoAdmin(admin.ModelAdmin):
     list_display = ('brand', 'designed_by', 'version','date')
 
 
+class DeviceStatusAdmin(admin.ModelAdmin):
+    list_display = ('first_check', 'second_check', 'allowed_adress__local')
+
 admin.site.register(SystemInfo, SystemInfoAdmin)
+admin.site.register(DeviceStatus, DeviceStatusAdmin)
