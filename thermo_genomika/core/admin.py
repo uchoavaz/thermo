1f2bcc131f4f586cfcf8533ad39b4ec114c3279f
joao uchoa <joao.victor@genomika.com.br>
2018-03-12 15:51:30 +0000

    correcao

--- thermo_genomika/core/admin.py
+++ thermo_genomika/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import SystemInfo
+from .models import DeviceStatus
 
 class SystemInfoAdmin(admin.ModelAdmin):
     list_display = ('brand', 'designed_by', 'version','date')
