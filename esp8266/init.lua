65b8b0a41ef8088a8af0b8ac9ab2e0a2925d5613
joao uchoa <joao.victor@genomika.com.br>
2018-07-10 18:12:58 +0000

    Update init.lua

--- esp8266/init.lua
+++ esp8266/init.lua
@@ -3,28 +3,28 @@ function read_analog ()
   a = r * 310,3030303 / 1024
   conn=net.createConnection(net.TCP, 0)
   conn:on("receive", function(conn, payload) print(payload) end)
-  conn:connect(8005, "172.16.225.9")
+  conn:connect(8003, "172.16.225.17")
   temp = "temp="..a
-  var="GET /catcher?"..temp.." HTTP/1.1\r\nHost: 172.16.225.9\r\nConnection: keep-alive\r\nAccept: */*\r\n\r\n"
+  var="GET /catcher?"..temp.." HTTP/1.1\r\nHost: 172.16.225.17\r\nConnection: keep-alive\r\nAccept: */*\r\n\r\n"
   conn:send(var)
   print(a)
-  gpio.write(3, gpio.HIGH)
-  gpio.write(3, gpio.LOW)
+
 end
 
 
 print("Ready to Set up wifi mode")
 wifi.setmode(wifi.STATION)
-gpio.mode(2, gpio.OUTPUT)
-gpio.mode(3, gpio.OUTPUT)
-gpio.write(3, gpio.LOW)
+gpio.mode(0, gpio.OUTPUT)
+gpio.mode(1, gpio.OUTPUT)
+gpio.write(0, gpio.LOW)
+gpio.write(1, gpio.LOW)
 
 
-ssid = "GNMK-ADMIN1"
-psw = "g3n3t1c@g3n0m1k@"
-ip = "172.16.225.190"
+ssid = "GNMK-LAB1"
+psw = "sc1m0n3g"
+ip = "172.16.240.40"
 mask = "255.255.255.0"
-gate = "172.16.225.1"
+gate = "172.16.240.1"
 
 wifi.sta.config(ssid, psw)
 wifi.sta.setip({ip=ip,netmask=mask,gateway=gate})
@@ -33,9 +33,12 @@ local cnt = 0
 tmr.alarm(0, 2000, 1, function()
    if (wifi.sta.status() == 5) then
       print("Config done, IP is "..wifi.sta.getip())
-      gpio.write(2, gpio.HIGH)
+      gpio.write(0, gpio.HIGH)
+      gpio.write(1, gpio.LOW)
       tmr.alarm(0, 300000, 1, read_analog)
    else
+      gpio.write(1, gpio.HIGH)
+      gpio.write(0, gpio.LOW)
       print("Trying to connect")
    end
   end)
